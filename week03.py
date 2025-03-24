eaa8ad5a922f2a7896cdb373212377a3b6121f6f
kjs-sr <kjs112541@naver.com>
2025-03-24 17:39:03 +0900

    v0.7 zip

--- week03.py
+++ week03.py
@@ -1,14 +1,6 @@
-def move_zeros(l):
-    zero_idx = 0
-    for i in range(len(l)):
-        n = l[i]
-        if n != 0:
-            l[zero_idx] = n
-            if zero_idx != i:
-                l[i] = 0
-            zero_idx = zero_idx + 1
-    return l
+groups = ['HOT', 'Seventenn', 'Black Pink', 'NJZ']
+#rating = [1,2,4,3,100]
+rating = [1,2,4,3]
 
-l = [99, 0, -7, 0, 16]
-move_zeros(l)
-print(l)
+group_rating = list(zip(groups,rating))
+print(group_rating)
