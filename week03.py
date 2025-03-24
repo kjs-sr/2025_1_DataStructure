6601ffe135b7c4065e874c17bf28719f7bc8d086
kjs-sr <kjs112541@naver.com>
2025-03-24 18:12:26 +0900

    v1.1 array

--- week03.py
+++ week03.py
@@ -1,9 +1,9 @@
 def inters(l1, l2):
-    l3 = []
-    for v in l1:
-        if v in l2:
-            l3.append(v)
-    return l3
+    s1 = set(l1)
+    s2 = set(l2)
+    return list(s1 & s2) #교집합
+    #return list(s1 | s2) #합집합
+    #return list(s1 - s2) #차집합
 
 l1 = [45, 5, 22, 31, 7, 19]
 l2 = [22, 1, 5, 2, 7, 28, 27, 19, 13, 41]
