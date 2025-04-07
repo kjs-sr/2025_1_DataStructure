bd605d0e151dfb57af4de86980c867804b898923
kjs <kjs12541@naver.com>
2025-04-07 18:18:00 +0900

    v2.4 stack

--- week05.py
+++ week05.py
@@ -1,11 +1,18 @@
-s1 = []
-s1.append("Data structure")
-s1.append("Database")
-print(s1[-1])
-print(s1)
-print(s1.pop())
-print(s1)
-print(s1.pop())
-print(s1)
-print(s1.pop())
-print(s1)
+#print(1+2))
+def is_valid_parentheses(expression : str) -> bool: #type hint
+    stack = list()
+    for letter in expression:
+        if letter == "(":
+            stack.append(letter)
+        if letter == ")":
+            if len(stack) == 0:
+                return False # )1+2(), (1+2))
+            else:
+                stack.pop()
+    return len(stack) == 0 # (1+2), ((3*2)/2), ((3*2/2)
+
+print(is_valid_parentheses(")1+2()"))
+print(is_valid_parentheses("(1+2))"))
+print(is_valid_parentheses("(1+2)"))
+print(is_valid_parentheses("((3*2)/2)"))
+print(is_valid_parentheses("((3*2/2)"))
