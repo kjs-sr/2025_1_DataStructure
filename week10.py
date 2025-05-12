26ec7303292a688a82f747f6d2d7e2296bd16601
kjs <kjs12541@naver.com>
2025-05-12 16:53:00 +0900

    v3.6 tree

--- week10.py
+++ week10.py
@@ -46,8 +46,7 @@ def insert(root, value):
             current = current.right  # 이동
     return root
 
-def search():
-    find_number = int(input("찾는 수는? "))
+def search(find_number):
     current = root
     while True:
         if find_number == current.data:
@@ -74,4 +73,5 @@ if __name__ == "__main__":
     print('BST 구성 완료')
     post_order(root)
     print()
-    search()
+    find_number = int(input("찾는 수는? "))
+    search(find_number)
