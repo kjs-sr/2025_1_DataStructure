7143e1eb7044b9c3f11c33114a6575504c7ca8d4
kjs <kjs12541@naver.com>
2025-05-12 17:44:18 +0900

    v3.8 tree

--- week10.py
+++ week10.py
@@ -25,6 +25,20 @@ def in_order(node):
     print(node.data, end="->")
     in_order(node.right)
 
+def search(find_number):
+    current = root
+    while True:
+        if find_number == current.data:
+            return True
+        elif find_number < current.data:
+            if current.left is None:
+                return  False
+            current = current.left
+        else:
+            if current.right is None:
+                return False
+            current = current.right
+
 def insert(root, value):
     node = TreeNode()
     node.data = value
@@ -46,19 +60,22 @@ def insert(root, value):
             current = current.right  # 이동
     return root
 
-def search(find_number):
-    current = root
-    while True:
-        if find_number == current.data:
-            return True
-        elif find_number < current.data:
-            if current.left is None:
-                return  False
-            current = current.left
-        else:
-            if current.right is None:
-                return False
-            current = current.right
+def delete(node, value):
+    if node is None:
+        return None
+
+    if value < node.data:
+        node.left = delete(node.left, value)
+    elif value > node.data:
+        node.right = delete(node.right, value)
+    else: # 같은 경우, 삭제할 노드를 찾음
+        # leaf 노드거나 자식이 1개 인 경우의 노드를 삭제
+        if node.left is None:
+            return node.right
+        elif node.right is None:
+            return node.left
+    return node
+
 
 if __name__ == "__main__":
     numbers = [10, 15, 8, 3, 9, 100, 7, 13]
@@ -70,8 +87,22 @@ if __name__ == "__main__":
     print('BST 구성 완료')
     post_order(root)
     print()
+    in_order(root)
+    print()
+    pre_order(root)
+    print()
+
     find_number = int(input("찾는 수는? "))
     if search(find_number):
         print(f"{find_number}을(를) 찾았습니다")
     else:
         print(f"{find_number}이(가) 존재하지 않습니다")
+
+    delete_number = int(input("제거할 숫자는? "))
+    root = delete(root, delete_number)
+    post_order(root)
+    print()
+    in_order(root)
+    print()
+    pre_order(root)
+    print()
