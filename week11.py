a0e2cf589bbce306313facdb19c884b2f9ef0872
kjs-sr <kjs12541@naver.com>
2025-05-19 16:48:52 +0900

    v4.1 graph

--- week11.py
+++ week11.py
@@ -17,3 +17,15 @@ for r in range(G1.size):
     for c in range(G1.size):
         print(G1.graph[r][c], end=' ')
     print()
+
+G2 = Graph(4)
+
+# 0 == A, 1 == B, 2 == C, 3 == D
+G2.graph[0][1] = 1; G2.graph[0][2] = 1
+G2.graph[3][0] = 1; G2.graph[3][2] = 1
+
+print("G2 방향 그래프")
+for r in range(G2.size):
+    for c in range(G2.size):
+        print(G2.graph[r][c], end=' ')
+    print()
