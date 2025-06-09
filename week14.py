b880da940aa12b1f0cf4bb8e5c45dfa629cfb148
kjs-sr <kjs12541@naver.com>
2025-06-09 17:49:17 +0900

    v5.5 BAEKJOON 10828

--- week14.py
+++ week14.py
@@ -1,16 +1,26 @@
-import math
-def is_prime(n) -> bool:
-  if n <= 1:
-      return False
-  elif n%2==0:
-      return False
-  else:
-    for i in range(3, int(math.sqrt(n))+1):
-        if n % i == 0:
-          return False
-  return True
+import sys
+n = int(input())
+stack = []
 
-s, e = map(int, input().split())
-for i in range(s, e+1):
-    if is_prime(i):
-        print(i)
+for i in range(n):
+  order = sys.stdin.readline().strip()
+  if 'push' in order:
+    number = order.split()
+    stack.append(number[-1])
+  elif order == 'pop':
+    if len(stack) == 0:
+      print(-1)
+    else:
+      print(stack.pop())
+  elif order == 'size':
+    print(len(stack))
+  elif order == 'empty':
+    if len(stack) == 0:
+      print(1)
+    else:
+      print(0)
+  elif order == 'top':
+    if len(stack) == 0:
+      print(-1)
+    else:
+      print(stack[-1])
