b0edcf5fa168103caecf983a3aa21813fa7fbe73
kjs-sr <kjs12541@naver.com>
2025-06-09 17:05:36 +0900

    v5.1 BAEKJOON

---
+++ week14.py
@@ -0,0 +1,14 @@
+import math
+def is_prime(n) -> bool:
+    if n <= 1:
+        return False
+    else:
+        for i in (2,int(math.sqrt(n))+1):
+            if n % i == 0:
+                return False
+    return True
+
+s,e = map(int, input().split())
+for i in range(s, e+1):
+    if is_prime(i):
+        print(i)
