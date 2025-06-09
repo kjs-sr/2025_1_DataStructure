5b765cbe4c12b7f12a3b77276c06e4a858665268
kjs-sr <kjs12541@naver.com>
2025-06-09 17:31:15 +0900

    v5.4 BAEKJOON

--- week14.py
+++ week14.py
@@ -5,7 +5,7 @@ def is_prime(n) -> bool:
   elif n%2==0:
       return False
   else:
-    for i in range(3, int(math.sqrt(n))+1):
+    for i in range(3, int(math.sqrt(n))+1, 2):
         if n % i == 0:
           return False
   return True
